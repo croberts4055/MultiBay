10f6ffd129ae30548778a71e893d7bf50947c927
Christopher Roberts <croberts4055@gmail.com>
2018-01-26 00:30:24 -0500

    Changed app name and added new features and slight styling

--- script.js
+++ script.js
@@ -9,9 +9,26 @@
             var title = item.title;
             var pic = item.galleryURL;
             var viewitem = item.viewItemURL;
+            var selling = item.sellingStatus[0].currentPrice[0]['__value__']
+            var startTime = item.listingInfo[0].startTime;
+            var endTime = item.listingInfo[0].endTime;
+            var shippingInfo = item.shippingInfo[0]
+            var listingInfo = item.listingInfo[0]
+            var timeLeft = item.watchCount;
+
+
+
             if (null != title && null != viewitem) {
+                var searchNum = document.getElementById('searchNumber').value;
+
+
+
                 html.push('<tr><td>' + '<img src="' + pic + '" border="0">' + '</td>' +
-                    '<td><a href="' + viewitem + '" target="_blank">' + title + '</a></td></tr>');
+                    '<td><a href="' + viewitem + '" target="_blank">' + title + '</a></td>' + '<td>' + "Unit Price:" + selling +
+                    '<br>' + "Start Time: " + startTime +
+                    '<br>' + "End Time: " + endTime +
+                    '<br>' + "Time Left: " + timeLeft + '</td></tr>');
+
             }
         }
         html.push('</tbody></table>');
@@ -70,15 +87,64 @@
 
     function ebayStart() {
 
-        var productName = document.getElementById('productName').value;
+        var productName = document.getElementById('productName').value.toString();
         var searchNum = document.getElementById('searchNumber').value.toString();
+        var country = document.getElementById('country').value.toString();
+
+
+
+        if (country == "United States") {
+            country = "US";
+        } else if (country == "Australia") {
+            country = "AU";
+        } else if (country == "Austria") {
+            country = "AT";
+        } else if (country == "Belgium(Dutch)") {
+            country = "NLBE";
+        } else if (country == "Belgium(French)") {
+            country = "FRBE";
+        } else if (country == "Canada(English)") {
+            country = "ENCA";
+        } else if (country == "Canada(French)") {
+            country = "FRCA";
+        } else if (country == "France") {
+            country = "FR";
+        } else if (country == "Germany") {
+            country = "DE";
+        } else if (country == "Hong Kong") {
+            country = "HK";
+        } else if (country == "India") {
+            country = "IN";
+        } else if (country == "Ireland") {
+            country = "IE";
+        } else if (country == "Italy") {
+            country = "IT";
+        } else if (country == "Malaysia") {
+            country = "MY";
+        } else if (country == "Motors") {
+            country = "MOTOR";
+        } else if (country == "Netherlands") {
+            country = "NL";
+        } else if (country == "Philippines") {
+            country = "PH";
+        } else if (country == "Poland") {
+            country = "PL";
+        } else if (country == "Singapore") {
+            country = "SG";
+        } else if (country == "Spain") {
+            country = "ES";
+        } else if (country == "Switzerland") {
+            country = "CH";
+        } else if (country == "United Kingdom") {
+            country = "GB";
+        }
+
         // Construct the request
-        // Replace MyAppID with your Production AppID
         var url = "http://svcs.ebay.com/services/search/FindingService/v1";
         url += "?OPERATION-NAME=findItemsByKeywords";
         url += "&SERVICE-VERSION=1.0.0";
-        url += "&SECURITY-APPNAME="; // Insert Key HERE!
-        url += "&GLOBAL-ID=EBAY-US";
+        url += "&SECURITY-APPNAME=Christop-Marketpl-PRD-951ca6568-761cc9c7";
+        url += "&GLOBAL-ID=EBAY-" + country;
         url += "&RESPONSE-DATA-FORMAT=JSON";
         url += "&callback=_cb_findItemsByKeywords";
         url += "&REST-PAYLOAD";
@@ -93,4 +159,32 @@
         s.src = url;
 
         document.body.appendChild(s);
+
+
+
+    }
+
+    function amazonSearch() {
+
+        var productName2 = document.getElementById('productName').value.toString();
+        if (productName2 == "") {
+            return;
+        } else {
+            var searchURL = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=" + productName2;
+            window.open(searchURL);
+        }
+
+    }
+
+    function youtubeSearch() {
+        var productName3 = document.getElementById('productName').value.toString();
+
+        if (productName3 == "") {
+            return;
+        } else {
+            var searchURL2 = "https://www.youtube.com/results?search_query=" + productName3 + " review";
+            window.open(searchURL2);
+        }
+
+
     }
